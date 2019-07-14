3241665af46d2c427f1743cce62c20cb1b47c375
ORHAN AKGÜL <orhan.akgull@gmail.com>
2019-07-14 14:13:38 +0300

    javascript yazıldı

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+function degis(){
+  let sayi = document.getElementById('sayi').value;
+  let output = document.getElementById('output');
+  let result = sayi > 18 ? 'büyük' : 'küçük';
+  output.innerText = result;
+}
+
+consolo.log('merhaba bu örnek bir projedir');
+consolo.log('merhaba Ahmet');
+consolo.log('Mrhaba murat');
